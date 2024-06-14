525a65727d046c6cd08e8e827bb1bd569a681ada
Felix G <felixglitsch@gmail.com>
2024-06-14 18:10:08 +0200

    allow simulation to record observables

--- src/simulation.jl
+++ src/simulation.jl
@@ -16,14 +16,18 @@ function update_histogram!(hist::Histogram, sys::GCMC_System)
 	hist.count += 1
 end
 
-function simulate(sys::GCMC_System, steps::Int64, therm_steps::Int64)
+function simulate(sys::GCMC_System, steps::Int64, therm_steps::Int64, sample_interval::Int64=1000; obs::Vector=[])
+	obs_vals = zeros(ceil(Int, steps/sample_interval), length(obs))
 	hist = Histogram(sys, dx=sys.dx)
 	for i in 1:therm_steps
 		step!(sys)
 	end
 	for i in 1:steps
 		step!(sys)
-		update_histogram!(hist, sys)
+		if i % sample_interval == 0
+			obs_vals[ceil(Int, i/sample_interval), :] = [o(sys) for o in obs]
+			update_histogram!(hist, sys)
+		end
 	end
-	return hist.ρ/hist.count
+	return hist.ρ/hist.count/(sys.dx^2), obs_vals
 end
