d7e3730fad49fa89ae1c337cf50c52a97c1985b5
Felix G <felixglitsch@gmail.com>
2024-06-14 13:35:13 +0200

    improved visualisation

--- src/GCMC_2D_Fluids.jl
+++ src/GCMC_2D_Fluids.jl
@@ -1,7 +1,8 @@
 module GCMC_2D_Fluids
 
 # Write your package code here.
-export GCMC_System, show
+export GCMC_System, show, simulate, Histogram, GCMC_Simulation, try_insert!, try_delete!, try_move!, energy
+
 include("structs.jl")
 include("utils.jl")
 include("sim_utils.jl")
--- src/utils.jl
+++ src/utils.jl
@@ -2,7 +2,7 @@ using Plots
 import Base: show
 function show(io::IO, sys::GCMC_System) # FIXME does not work properly yet with periodic boundary conditions
 	p = plot(legend=false, xlim=(-sys.σ/2, sys.L+sys.σ/2), ylim=(-sys.σ/2, sys.L+sys.σ/2), size=(500, 500))
-	for pos in sys.positions
+	for pos in reverse.(sys.positions)
 		scatter!(p, [pos[1]], [pos[2]], markercolor=:blue, markersize=sys.σ/sys.L*230)
 		if pos[1] < sys.σ/2
 			scatter!(p, [pos[1]+sys.L], [pos[2]], markercolor=:blue, markersize=sys.σ/sys.L*230)
@@ -17,8 +17,8 @@ function show(io::IO, sys::GCMC_System) # FIXME does not work properly yet with 
 			scatter!(p, [pos[1]], [pos[2]-sys.L], markercolor=:blue, markersize=sys.σ/sys.L*230)
 		end
 	end
-			# p = plot!(p, [0, sys.L, sys.L, 0, 0], [0, 0, sys.L, sys.L, 0], linewidth=2, linecolor=:black)
-			p = plot!(p, [0-sys.σ/4, sys.L+sys.σ/4, sys.L+sys.σ/4, -sys.σ/4, -sys.σ/4], [-sys.σ/4, -sys.σ/4, sys.L+sys.σ/4, sys.L+sys.σ/4, -sys.σ/4], linewidth=sys.σ/sys.L*230, linecolor=:black)
+	# draw box
+	p = plot!(p, [0-sys.σ/4, sys.L+sys.σ/4, sys.L+sys.σ/4, -sys.σ/4, -sys.σ/4], [-sys.σ/4, -sys.σ/4, sys.L+sys.σ/4, sys.L+sys.σ/4, -sys.σ/4], linewidth=sys.σ/sys.L*230, linecolor=:black)
 
 	display(p)
 end
