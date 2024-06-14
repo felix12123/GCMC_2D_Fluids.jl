c7b65653556b776efd51e62e5fa8b29ee6ce306b
Felix G <felixglitsch@gmail.com>
2024-06-14 12:39:41 +0200

    fixed boundary problem

--- src/utils.jl
+++ src/utils.jl
@@ -1,7 +1,7 @@
 using Plots
 import Base: show
 function show(io::IO, sys::GCMC_System) # FIXME does not work properly yet with periodic boundary conditions
-	p = plot(legend=false, xlim=(-0, sys.L), ylim=(-0, sys.L), size=(500, 500))
+	p = plot(legend=false, xlim=(-sys.σ/2, sys.L+sys.σ/2), ylim=(-sys.σ/2, sys.L+sys.σ/2), size=(500, 500))
 	for pos in sys.positions
 		scatter!(p, [pos[1]], [pos[2]], markercolor=:blue, markersize=sys.σ/sys.L*230)
 		if pos[1] < sys.σ/2
@@ -17,5 +17,8 @@ function show(io::IO, sys::GCMC_System) # FIXME does not work properly yet with 
 			scatter!(p, [pos[1]], [pos[2]-sys.L], markercolor=:blue, markersize=sys.σ/sys.L*230)
 		end
 	end
+			# p = plot!(p, [0, sys.L, sys.L, 0, 0], [0, 0, sys.L, sys.L, 0], linewidth=2, linecolor=:black)
+			p = plot!(p, [0-sys.σ/4, sys.L+sys.σ/4, sys.L+sys.σ/4, -sys.σ/4, -sys.σ/4], [-sys.σ/4, -sys.σ/4, sys.L+sys.σ/4, sys.L+sys.σ/4, -sys.σ/4], linewidth=sys.σ/sys.L*230, linecolor=:black)
+
 	display(p)
 end
