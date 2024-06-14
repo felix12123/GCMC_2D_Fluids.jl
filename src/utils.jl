f16a7ecf8d80cdd87f69aa8fb534d55646a589f6
Felix G <felixglitsch@gmail.com>
2024-06-14 18:09:51 +0200

    improve show

--- src/utils.jl
+++ src/utils.jl
@@ -1,20 +1,21 @@
 using Plots
 import Base: show
 function show(io::IO, sys::GCMC_System) # FIXME does not work properly yet with periodic boundary conditions
+	ms = sys.σ/sys.L*220
 	p = plot(legend=false, xlim=(-sys.σ/2, sys.L+sys.σ/2), ylim=(-sys.σ/2, sys.L+sys.σ/2), size=(500, 500))
 	for pos in reverse.(sys.positions)
-		scatter!(p, [pos[1]], [pos[2]], markercolor=:blue, markersize=sys.σ/sys.L*230)
+		scatter!(p, [pos[1]], [pos[2]], markercolor=:blue, markersize=ms)
 		if pos[1] < sys.σ/2
-			scatter!(p, [pos[1]+sys.L], [pos[2]], markercolor=:blue, markersize=sys.σ/sys.L*230)
+			scatter!(p, [pos[1]+sys.L], [pos[2]], markercolor=:blue, markersize=ms)
 		end
 		if pos[1] > sys.L - sys.σ/2
-			scatter!(p, [pos[1]-sys.L], [pos[2]], markercolor=:blue, markersize=sys.σ/sys.L*230)
+			scatter!(p, [pos[1]-sys.L], [pos[2]], markercolor=:blue, markersize=ms)
 		end
 		if pos[2] < sys.σ/2
-			scatter!(p, [pos[1]], [pos[2]+sys.L], markercolor=:blue, markersize=sys.σ/sys.L*230)
+			scatter!(p, [pos[1]], [pos[2]+sys.L], markercolor=:blue, markersize=ms)
 		end
 		if pos[2] > sys.L - sys.σ/2
-			scatter!(p, [pos[1]], [pos[2]-sys.L], markercolor=:blue, markersize=sys.σ/sys.L*230)
+			scatter!(p, [pos[1]], [pos[2]-sys.L], markercolor=:blue, markersize=ms)
 		end
 	end
 	# draw box
