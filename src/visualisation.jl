436d09f4dd5b8956c7e48816ab5591eff256a9ed
felix12123 <felixglitsch@gmail.com>
2024-11-13 00:13:23 +0100

    update visualisation

--- src/visualisation.jl
+++ src/visualisation.jl
@@ -1,4 +1,20 @@
-function plot_data_folder(folder::String, plot_folder::String=folder*"/plots"; project=false)
+using Plots, DelimitedFiles, ProgressMeter
+
+function plot_file(file::String)
+	d = readdlm(file, ';')
+	L = Int(sqrt(size(d, 1)))
+	rho = reshape(d[:, 2], L, L)
+	c1 = reshape(d[:, 1], L, L)
+	V = c1 .- log.(rho)
+
+   plt_r = heatmap(rho, aspect_ratio=1, title="Density")
+	plt_c = heatmap(c1, aspect_ratio=1, title="c1")
+	plt_V = heatmap(V, aspect_ratio=1, title="Potential")
+	plt = plot(plt_r, plt_c, plt_V, layout=(1, 3), size=(1200, 400))
+	
+	return plt
+end
+function plot_data_folder(folder::String, plot_folder::String=folder*"/plots")
 	files = filter(x->occursin(".dat", x), readdir(folder, join=true))
 	if isempty(files)
 		println("No data files found in folder $folder")
@@ -8,23 +24,9 @@ function plot_data_folder(folder::String, plot_folder::String=folder*"/plots"; p
 		mkdir(plot_folder)
 	end
 
-	function plot_rho(rho)
-		if project
-			rho_1d = mean(rho, dims=2)
-			return plot(rho_1d, xlabel="x", ylabel="ρ(x)", legend=false)
-		else
-			return heatmap(rho, aspect_ratio=1)
-		end
-	end
-	
-	for file in files
-		d = readdlm(file, ';')
-		L = Int(sqrt(size(d, 1)))
-		rho = reshape(d[:, 2], L, L)
-		c1 = reshape(d[:, 1], L, L)
-		plt_r = plot_rho(rho)
-		plt_c = plot_rho(c1)
-		plt = plot(plt_r, plt_c, layout=(1, 2), size=(800, 400))
+	folder_name = split(folder, "/")[end]
+	@showprogress "Plotting folder $(folder_name)" for file in files
+		plt = plot_file(file)
 		savefig(plt, plot_folder*"/$(basename(file)[1:end-4]).png")
 	end
 	
